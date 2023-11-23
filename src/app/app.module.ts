e12ac2f828428e093d89ceb8aa13ac5da1e10a15
JmLizarragaNodos <jlizarraga@nodos.cl>
2023-11-23 10:13:30 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { VentaComponent } from './Componentes/venta/venta.component';
+import { SaldoInventarioComponent } from './Componentes/saldo-inventario/saldo-inventario.component';
+import { ProductoComponent } from './Componentes/producto/producto.component';
+import { ClienteComponent } from './Componentes/Personas/cliente/cliente.component';
+import { UsuarioComponent } from './Componentes/Personas/usuario/usuario.component';
+import { MarcaComponent } from './Componentes/marca/marca.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VentaComponent,
+    SaldoInventarioComponent,
+    ProductoComponent,
+    ClienteComponent,
+    UsuarioComponent,
+    MarcaComponent
   ],
   imports: [
     BrowserModule,
